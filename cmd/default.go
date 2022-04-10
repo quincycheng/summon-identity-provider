46e38c99a4df30dd57b5661818b9e65646b23e9f
Quincy Cheng <quincy.cheng@gmail.com>
2022-04-10 14:26:17 +0800

    Updated default messages

--- cmd/default.go
+++ cmd/default.go
@@ -21,13 +21,13 @@ func PrintIntroMessage() {
 	fmt.Println("To get started, you can configure the provider by executing:")
 	fmt.Println(highlightStyle.Render("\tsummon-identity-provider --config"))
 	fmt.Println("")
-	fmt.Println("To generate sample usage & instructions based on your config, execute:")
-	fmt.Println(highlightStyle.Render("\tsummon-identity-provider --usage"))
+	fmt.Println("To login to CyberArk Identity, execute:")
+	fmt.Println(highlightStyle.Render("\tsummon-identity-provider --login"))
 	fmt.Println("")
 	fmt.Println("Usage:")
 
 	fmt.Printf("\t%s\t\t\t Print this message\n", highlightStyle.Render("summon-identity-provider"))
-	fmt.Printf("\t%s\t Fetch a password of Secured Item from CyberArk Identity\n", highlightStyle.Render("summon-identity-provider <Secured Item Name>"))
+	fmt.Printf("\t%s\t Fetch the content of secured item from CyberArk Identity\n", highlightStyle.Render("summon-identity-provider <Secured Item Name>"))
 	fmt.Printf("\t%s\t\t Start configuration wizard\n", highlightStyle.Render("summon-identity-provider --config"))
 	fmt.Printf("\t%s\t\t Login to CyberArk Identity\n", highlightStyle.Render("summon-identity-provider --login"))
 
